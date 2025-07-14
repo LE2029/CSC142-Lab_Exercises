4f4854394e4fc002337e5edc4042439bb72b0af7
LE2029 <11009410h@gmail.com>
2025-07-13 22:26:36 -0700

    Add files via upload

---
+++ CSC142-Lab_Exercise1.py
@@ -0,0 +1,8 @@
+
+print("========")
+print(" CSC142")
+print("========")
+print("")
+print("One thing I learned:")
+print("")
+print("Grading Breakdown: Projects: 50%  Exams: 35.7%  Labs: 7.1%  Quizzes: 3.6%  Participation: 3.6%")
