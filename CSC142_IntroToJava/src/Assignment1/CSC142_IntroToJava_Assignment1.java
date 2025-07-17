8dc40ce31d578f5003c7f926a87bb05443e04239
Horry <Horry.ho.ren@gmail.com>
2025-07-17 06:46:50 -0700

    Assignment 1: Eclipse Setup and Java Practice

---
+++ CSC142_IntroToJava/src/Assignment1/CSC142_IntroToJava_Assignment1.java
@@ -0,0 +1,20 @@
+package Assignment1;
+
+public class CSC142_IntroToJava_Assignment1 {
+	
+	public static void main(String[] args) {
+
+		System.out.println("CSC142 - Intro to Java");
+		
+		System.out.println("");
+		
+		System.out.println("What I learned from Module 1: ");
+				
+		System.out.println("The benefits of using Java includes "
+				+ "it's strong community and vast amounts of documentation as well as it's "
+				+ "platform independance via JVM.");
+		
+	}
+
+}
+
